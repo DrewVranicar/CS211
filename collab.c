06a5d743eba099c8b513db1b89c99e9d41f980a6
Drew Vranicar <dvran2@uic.edu>
2022-11-15 17:25:56 -0600

    first commit

--- collab.c
+++ collab.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
 
+void dvran2_itiwar4() {
+  printf("Drew Vranicar and Ishita Tiwari\n");
+  printf("We learned about all the different git commands including how to clone a git repository.\n");
+}
 
-
-int main() {
-printf("clone");
-return 0;
+int main()
+{
+    
+    dvran2_itiwar4();
+    
+    return 0;
 }
